30a67707a3fc3f66b3ae883d5644f704b54161b8
Jonathan Rose <jonathan_rose@outlook.com>
2023-10-05 12:06:48 +0100

    Reorganised project structure to use src directory.

--- src/alphabet/core.clj
+++ src/alphabet/core.clj
@@ -1,7 +1,29 @@
 (ns alphabet.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def fridge "aaaaaabbcccddddeeeeeeffgghhhiiiiijjkkllllmmmmmnnnoooopppqqrrrrssssttttuuuuvvwxxyyzz")
+
+(def test-letters "All big cats do eat fresh gazelle.")
+
+(defn leftover-letters
+  "Takes two strings.
+   Subtracts the frequency of letters in the second string from the frequency of letters in the first. 
+   Prints the second string and what letters remain of the first."
+  [letters sentence]
+  (let [frequency (let [letters-freq   (-> letters
+                                           (str/lower-case)
+                                           (str/replace #"[^a-z]" "")
+                                           (vec)
+                                           (frequencies))
+                        sentence-freq  (-> sentence
+                                           (str/lower-case)
+                                           (str/replace #"[^a-z]" "")
+                                           (vec)
+                                           (frequencies))
+                        remaining-freq (merge-with - letters-freq sentence-freq)] remaining-freq)]
+    (if (some #(neg? %) (vals frequency))
+      (println "Insufficient letters on the fridge.")
+      (do (println "Your sentence is: " sentence)
+          (println "Remaining letters: " (apply str (for [[char count] frequency]
+                                                      (apply str (repeat count char)))))))))
